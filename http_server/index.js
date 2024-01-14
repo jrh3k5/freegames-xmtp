eed16154f33cce261eef459139107382c5601b16
Joshua Hyde <jrh3k5@gmail.com>
2024-01-14 07:14:38 -0600

    Add health check

--- http_server/index.js
+++ http_server/index.js
@@ -13,6 +13,10 @@ export function buildWebhookServer(webhookSecret, freestuffClient, notifier) {
     const webhookHandler = NewWebhookHandler(webhookSecret, freestuffClient, notifier);
     
     app.post("/freestuffbot.xyz/webhook", webhookHandler);
+    app.get("/_health", (_, res) => {
+        res.status(200);
+        res.end();
+    });
 
     return httpServer;
 }
