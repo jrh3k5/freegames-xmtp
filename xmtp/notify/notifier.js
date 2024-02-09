7e0011d8aa83a23643da35682a4664a6cbb2b3ce
Joshua Hyde <jrh3k5+github@gmail.com>
2024-02-08 22:20:25 -0600

    Don't include price if there is none (#34)

--- xmtp/notify/notifier.js
+++ xmtp/notify/notifier.js
@@ -7,7 +7,10 @@ export class Notifier {
 
     async notify(recipientAddress, gameDetails) {
         const storeName = getHumanReadableStoreName(gameDetails.store);
-        const message = `Free game (originally \$${gameDetails.originalPrice}): ${gameDetails.gameTitle}\n\nGet it from ${storeName} here: ${gameDetails.url}\n\nIf this link doesn't open properly in your app's embedded browser, try copying the link directly into your phone's browser.`;
+        let message = `${gameDetails.gameTitle}\n\nGet it from ${storeName} here: ${gameDetails.url}\n\nIf this link doesn't open properly in your app's embedded browser, try copying the link directly into your phone's browser.`;
+        if (gameDetails.originalPrice) {
+            message = `Free game (originally \$${gameDetails.originalPrice}): ${message}`;
+        }
         const conversation = await this.xmtpClient.conversations.newConversation(recipientAddress);
         await conversation.send(message);
     }
