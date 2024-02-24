9ea8228fbc0903ba4138a270497840c63625437a
Joshua Hyde <jrh3k5+github@gmail.com>
2024-02-24 16:16:42 -0600

    Fix image caching (#61)

--- test/images/metadata/caching_retriever.test.js
+++ test/images/metadata/caching_retriever.test.js
@@ -1,55 +1,43 @@
 import { expect } from "chai";
 import { CachingRetriever } from "../../../images/metadata/caching_retriever.js";
+import { Metadata } from "../../../images/metadata/model.js";
+import { newCache } from "../../../cache/cache.js";
 
 describe("Caching Retriever", () => {
     let retriever;
-    let cachedData;
     let imageMetadata;
 
     beforeEach(() => {
         imageMetadata = {};
-        cachedData = {};
 
         const delegateRetriever = {};
         delegateRetriever.getMetadata = async (imageURL) => {
             return imageMetadata[imageURL];
         }
-
-        const cache = {};
-        cache.get = async (cacheKey) => {
-            return cachedData[cacheKey];
-        };
-        cache.set = async (cacheKey, data) => {
-            cachedData[cacheKey] = data;
-        };
-
+        
+        const cache = newCache();
         retriever = new CachingRetriever(delegateRetriever, cache);
-    })
-
-    describe("there is cached data", () => {
-        it("returns the cached data", async () => {
-            const imageURL = "https://test/cached.png";
-            const cachedImageMetadata = {
-                filename: "cached.png"
-            }
-            cachedData[`image-metadata-${imageURL}`] = cachedImageMetadata;
-
-            const returnedData = await retriever.getMetadata(imageURL);
-            expect(returnedData).to.equal(cachedImageMetadata);
-        })
     })
 
     describe("there is no cached data", () => {
         it("does a live pull and caches the data", async () => {
             const imageURL = "https://test/live.png";
-            const liveData = {
-                filename: "live.png"
-            };
+            const liveData = new Metadata("live.png", "image/png", new Uint8Array([65, 66, 67, 68]));
             imageMetadata[imageURL] = liveData;
 
             const returnedData = await retriever.getMetadata(imageURL);
-            expect(returnedData).to.equal(liveData);
-            expect(cachedData[`image-metadata-${imageURL}`]).to.equal(liveData);
+            expect(returnedData.filename).to.equal("live.png");
+            expect(returnedData.mimeType).to.equal("image/png");
+            expect(Buffer.from(returnedData.data).toString("base64")).to.equal("QUJDRA==");
+
+            // "Eliminate" the data from a live pull to ensure that a second pull retrieves the cached data
+            imageMetadata[imageURL] = undefined;
+
+            const cachedData = await retriever.getMetadata(imageURL);
+            expect(cachedData.filename).to.equal("live.png");
+            expect(cachedData.mimeType).to.equal("image/png");
+            expect(Buffer.from(cachedData.data).toString("base64")).to.equal("QUJDRA==");
+
         })
     })
 })
