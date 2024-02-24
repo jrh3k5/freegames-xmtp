22f380695e3537639d43f3971ccd48db331d57eb
Joshua Hyde <jrh3k5@gmail.com>
2024-02-24 16:13:56 -0600

    Fix image caching

--- images/metadata/caching_retriever.js
+++ images/metadata/caching_retriever.js
@@ -1,3 +1,5 @@
+import { Metadata } from "./model.js";
+
 // This is a wrapper implementation of Retriever used to provide caching.
 export class CachingRetriever {
     constructor(delegateRetriever, cache) {
@@ -10,11 +12,20 @@ export class CachingRetriever {
 
         const cached = await this.cache.get(cacheKey);
         if (cached != undefined) {
-            return cached;
+            const dataBytes = new Uint8Array(Buffer.from(cached.dataBase64, "base64"));
+
+            return new Metadata(cached.filename, cached.mimeType, dataBytes);;
         }
 
         const imageMetadata = await this.delegateRetriever.getMetadata(imageURL);
-        await this.cache.set(cacheKey, imageMetadata);
+
+        const cachedMetadata = {
+            filename: imageMetadata.filename,
+            mimeType: imageMetadata.mimeType,
+            dataBase64: Buffer.from(imageMetadata.data).toString("base64")
+        };
+        await this.cache.set(cacheKey, cachedMetadata);
+        
         return imageMetadata;
     }
 }
