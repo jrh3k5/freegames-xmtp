61669c857887269e29aa7b4d5b036fdbf1e2c91c
Joshua Hyde <jrh3k5@gmail.com>
2024-02-26 18:42:28 -0600

    Send user a greeting when they send registration funds

--- subscriptions/eth_send/handler.js
+++ subscriptions/eth_send/handler.js
@@ -1,8 +1,9 @@
 export class Handler {
-    constructor(subscriptionService, subscriptionDurationBlocks, minimumGwei) {
+    constructor(subscriptionService, subscriptionDurationBlocks, minimumGwei, xmtpClient) {
         this.subscriptionService = subscriptionService;
         this.subscriptionDurationBlocks = subscriptionDurationBlocks;
         this.minimumGwei = minimumGwei;
+        this.xmtpClient = xmtpClient;
     }
 
     async handle(senderAddress, blockNumber, amount) {
@@ -13,5 +14,9 @@ export class Handler {
         console.log(`${senderAddress} sent ${amount} gwei and would be subscribed for ${this.subscriptionDurationBlocks} blocks`);
 
         await this.subscriptionService.upsertSubscription(senderAddress, blockNumber + this.subscriptionDurationBlocks);
+
+        const conversation = await this.xmtpClient.conversations.newConversation(senderAddress);
+        const salutation = `Welcome to the free games XMTP bot! Your subscription will last until block ${blockNumber}`;
+        await conversation.send(salutation);
     }
 }
